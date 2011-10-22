b2a111267f3dcd06e587f59b8f2d4bb524dd2a46
Jerry Cheung <jch@whatcodecraves.com>
2011-10-21 23:35:19 -0400

    update README, add bash, and cleanup

--- setup.sh
+++ setup.sh
@@ -2,15 +2,23 @@
 
 DOTFILESDIR=$(pwd)
 
-# Emacs
+echo "Setting up bash..."
+rm -f ~/.profile
+ln -s $DOTFILESDIR/bash/profile ~/.profile
+
+echo "Setting up emacs..."
 rm -rf ~/.emacs ~/.emacs.d
 ln -s $DOTFILESDIR/emacs/emacs ~/.emacs
 ln -s $DOTFILESDIR/emacs/emacs.d ~/.emacs.d
 
-# Git
+echo "Setting up git..."
 rm -f ~/.gitconfig
 ln -s $DOTFILESDIR/gitconfig ~/.gitconfig
 
-# Screen
+echo "Setting up screen..."
 rm -f ~/.screenrc
 ln -s $DOTFILESDIR/screenrc ~/.screenrc
+
+echo "Setting up rdebug..."
+rm -f ~/.rdebugrc
+ln -s $DOTFILESDIR/ruby/rdebugrc ~/.rdebugrc
